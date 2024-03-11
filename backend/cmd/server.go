cbb65884c0107d2c52f2adddbfd245c2eda6633a
Nagisa-Ishikawa <nagisa.ishikawa@divx.co.jp>
2024-03-11 20:35:08 +0900

    echo導入

---
+++ backend/cmd/server.go
@@ -0,0 +1,31 @@
+
+package main
+
+import (
+	"net/http"
+	"aptitude-quiz-master/api"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+  // インスタンスを作成
+  e := echo.New()
+
+  // ミドルウェアを設定
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // ルートを設定
+  e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+  e.GET("/aaa", api.Aaa) 
+
+  // サーバーをポート番号1323で起動
+  e.Logger.Fatal(e.Start(":1323"))
+}
+
+// ハンドラーを定義
+func hello(c echo.Context) error {
+  return c.String(http.StatusOK, "Hello, World!")
+}
