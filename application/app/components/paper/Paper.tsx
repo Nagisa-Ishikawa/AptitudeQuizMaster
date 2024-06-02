4fc56b9ca8869b3a97e50049e17a659f32c49122
yoneda <chat.414.kome@gmail.com>
2024-06-02 16:12:29 +0900

    コンポーネント側でスタイリングを決めるようにする

--- application/app/components/paper/Paper.tsx
+++ application/app/components/paper/Paper.tsx
@@ -3,33 +3,19 @@ import { ReactNode } from "react";
 
 interface CustomPaperProps {
   children: ReactNode;
-  padding?: string;
-  margin?: string;
-  border?: string;
-  borderRadius?: string;
-  fontSize?: string;
-  maxWidth?: string;
 }
 
-const CustomPaper = ({
-  children,
-  padding = rem(40),
-  margin = `${rem(80)} auto`,
-  border = `${rem(2)} solid #0141A0`,
-  borderRadius = "0",
-  fontSize,
-  maxWidth = rem(1280),
-}: CustomPaperProps) => {
+const CustomPaper = ({ children }: CustomPaperProps) => {
   const theme = useMantineTheme();
   return (
     <Paper
       style={{
-        margin,
-        border,
-        padding,
-        borderRadius,
-        fontSize: fontSize || theme.fontSizes.md,
-        maxWidth,
+        margin: `${rem(80)} auto`,
+        border: `${rem(2)} solid #0141A0`,
+        padding: rem(40),
+        borderRadius: 0,
+        fontSize: theme.fontSizes.md,
+        maxWidth: rem(1280),
       }}
     >
       {children}
