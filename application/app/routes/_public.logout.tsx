e450e8cc6098f5c9058af223b354a88c5b7c17ed
Nagisa-Ishikawa <nagisa.ishikawa@divx.co.jp>
2024-10-23 21:24:51 +0900

    ログアウトなおした

--- application/app/routes/_public.logout.tsx
+++ application/app/routes/_public.logout.tsx
@@ -1,17 +1,6 @@
-import type {
-  ActionFunction,
-  ActionFunctionArgs,
-  LoaderFunction,
-} from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
 import { authenticator } from "../services/auth.server";
 
-export const action: ActionFunction = async ({
-  request,
-}: ActionFunctionArgs) => {
+export const loader: LoaderFunction = async ({ request }) => {
   return await authenticator.logout(request, { redirectTo: "/" });
-};
-
-export const loader: LoaderFunction = async () => {
-  return redirect("/");
 };
