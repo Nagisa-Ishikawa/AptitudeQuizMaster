0e0086d131902bbf0b5134c961eab48a7bed8c22
hedgehog248 <106587213+hedgehog248@users.noreply.github.com>
2024-10-31 22:52:12 +0900

    Iconコンポーネントにpropsを追加しておく

--- application/app/components/Icon/ArrowDownIcon.tsx
+++ application/app/components/Icon/ArrowDownIcon.tsx
@@ -3,11 +3,16 @@ import Icon from "../../../public/images/icons/keyboard_arrow_down.svg";
 
 import { Image, ThemeIcon } from "@mantine/core";
 
+type Props = {
+  size?: string;
+  [key: string]: unknown;
+};
+
 /** 下向きアイコン */
-export const ArrowDownIcon: React.FC = () => {
+export const ArrowDownIcon: React.FC<Props> = ({ size, ...props }: Props) => {
   return (
-    <ThemeIcon>
-      <Image src={Icon} alt="下向きアイコン" />
+    <ThemeIcon size={size} {...props}>
+      <Image w={size} h={size} src={Icon} alt="下向きアイコン" />
     </ThemeIcon>
   );
 };
--- application/app/components/Icon/ArrowUpIcon.tsx
+++ application/app/components/Icon/ArrowUpIcon.tsx
@@ -3,11 +3,16 @@ import Icon from "../../../public/images/icons/keyboard_arrow_up.svg";
 
 import { Image, ThemeIcon } from "@mantine/core";
 
+type Props = {
+  size?: string;
+  [key: string]: unknown;
+};
+
 /** 上向きアイコン */
-export const ArrowUpIcon: React.FC = () => {
+export const ArrowUpIcon: React.FC<Props> = ({ size, ...props }: Props) => {
   return (
-    <ThemeIcon>
-      <Image src={Icon} alt="上向きアイコン" />
+    <ThemeIcon size={size} {...props}>
+      <Image w={size} h={size} src={Icon} alt="上向きアイコン" />
     </ThemeIcon>
   );
 };
